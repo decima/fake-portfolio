8b8ef9bdcf19b4c0ddb5f615d837dece9ef7fb56
Coyls <loic.grecofr@gmail.com>
2021-02-19 17:52:42 +0100

    add : data modification

--- src/data.php
+++ src/data.php
@@ -3,19 +3,19 @@ $realisations = [
     [
         "title" => "Maya",
         "description" => "Dans le cadre du cours HTML/CSS, nous avons dû réaliser un site dans le thème marchand. Nous avions une semaine et le droit de n’utiliser uniquement les langages HTML et CSS. Le site devait être composé d’au minimum 3 pages et contenir des animations ainsi que des transitions, un formulaire et si possible le rendre responsive.",
-        "image" => "https://p4.wallpaperbetter.com/wallpaper/980/964/908/linux-minimalism-foxyriot-tux-wallpaper-preview.jpg",
+        "image" => "../src/images/maya.png",
         "link" => "https://coyls.github.io/maya/"
     ],
     [
         "title" => "Workshop",
         "description" => "Dans le cadre du premier workshop HTML/CSS,nous avions une semaine en autonomie pour copier et ajouter des éléments à un site réalisé en one-page, et adapter le site pour téléphone. Nous avions pour seul modèle une vidéo. Les src étaient fournies. Les contraintes étaient que nous avions le droit d’utiliser uniquement les langages HTML/CSS.",
-        "image" => "https://www.live-rates.com/system/blogs/attachments/000/000/026/original/php1.jpg?1547433041",
+        "image" => "../src/images/workshop.png",
         "link" => "https://coyls.github.io/workshop/"
     ],
     [
         "title" => "Kikk Festival",
         "description" => "Lors de leur workshop, les designers de notre promotion ont dû réaliser la maquette d’un site pour qu’il soit exploitable par un développeur. Dans l’objectif de travailler mes compétences en HTML/CSS/JS et de pouvoir tester de nouvelles choses. J’ai récupéré une maquette et l’ai intégrée.",
-        "image" => "https://i.pinimg.com/originals/0b/07/e6/0b07e6c038ceecfb02d3b96bda9c8738.png",
+        "image" => "../src/images/kikk-festival.png",
         "link" => "https://coyls.github.io/kikk-festival/"
     ],
 ];
@@ -51,15 +51,15 @@ $formation = [
 $passion = [
     [
         "passion" => "Cardistry",
-        "description" => "Description de la cardistry",
+        "description" => "La cardistry est l'art de la manipulation des cartes dans le but de cree des enchainements visuellement beaux et techniques.",
     ],
     [
         "passion" => "Voyage",
-        "description" => "Explication de l'imortance des voyage dans ma vie et ce qu'il mon apporter",
+        "description" => "Je voyage depuis que je suis né, j'ai pu découvrir de nombreuses cultures cela me permet d'avoir plus de recul sur notre monde.",
     ],
     [
         "passion" => "Musique",
-        "description" => "Explication de l'importance de la musique dans ma vie",
+        "description" => "  J'écoute de la musique en permanence. Principalement du métal et du rock. Elle me permet de rester concentré et de garder un rythme.",
     ],
 
 ];
